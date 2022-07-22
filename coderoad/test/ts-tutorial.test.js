042b2dad8a849dd28f44d7a82b96682cf6717d34
sandrameow <sandra.vlajinic@endava.com>
2022-07-22 15:01:51 +0200

    2.1

--- coderoad/test/ts-tutorial.test.js
+++ coderoad/test/ts-tutorial.test.js
@@ -15,3 +15,32 @@ describe("package.json", () => {
     );
   });
 });
+
+
+//second test
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
+const readdir = util.promisify(fs.readdir)
+const getRootDir = async (dir = process.cwd()) => {
+  const pathToRoot = path.join(dir, '..')
+  const rootDir = await readdir(pathToRoot)
+
+  if (!rootDir) {
+    throw new Error(`Could not find folder ${pathToRoot}`)
+  }
+
+  return rootDir
+}
+
+describe('ts-tutorialv2 folder', () => {
+  let rootDir
+  before(async () => {
+    rootDir = await getRootDir()
+  })
+
+  it('should have an app.ts file', async () => {
+    assert(rootDir.indexOf('app.ts') >= 0)
+  })
+})
